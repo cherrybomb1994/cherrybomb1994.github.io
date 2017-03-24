01f19e2ef1db87cc7abf6c72b548b221fca7a049
cherrybomb1994 <1070983213@qq.com>
2017-03-24 09:28:06 +0800

    Add files via upload

---
+++ yaoyao/03/biaodan.js
@@ -0,0 +1,39 @@
+var student = document.getElementById("student");
+var nostudent = document.getElementById("nostudent");
+var list = document.getElementById("select1");
+window.onload=function(){
+	document.getElementById("job").style.display="none";
+}
+nostudent.onclick=function(){
+	document.getElementById("school").style.display="none";
+	document.getElementById("job").style.display="";
+	
+}
+student.onclick=function(){
+	document.getElementById("school").style.display="";
+	document.getElementById("job").style.display="none";
+	
+}
+list.onclick=function(){
+	selectDistrict();
+}
+
+function selectDistrict() {
+    var data = {
+        beijing: ["北京邮电大学", "清华大学", "北京大学"],
+        shanghai: ["复旦大学", "上海交通大学", "同济大学"],
+        guangzhou: ["广州1大学", "广州2大学", "广州3大学"]
+    }
+    var source = document.getElementById("select1");
+    var target = document.getElementById("select2");
+    var selected = source.options[source.options.selectedIndex].value;
+	if(selected=="init0"){}
+	else{
+		target.innerHTML="";
+    for (var i = 0; i < data[selected].length; i++) {
+        var opt = document.createElement('option');
+		opt.innerHTML = data[selected][i];
+		document.getElementById('select2').appendChild(opt);
+      }
+	}
+}
